32c438a28cda4ae10ec5f6b8ae86e24124b179a4
Terence Faid JABO <faidterence@outlook.com>
2024-04-06 22:43:55 +0200

    feat(properties):get all properties functionality

--- src/main/java/com/faidterence/farmManagementSystem/controllers/PropertyController.java
+++ src/main/java/com/faidterence/farmManagementSystem/controllers/PropertyController.java
@@ -8,10 +8,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -37,6 +36,10 @@ public class PropertyController {
         responseDto.setDate(savedProperty.getDate());
 
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public List<Property> getAllProperties() {
+        return propertyService.getAllProperties();
     }
 
 }
