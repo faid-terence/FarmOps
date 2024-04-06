3cca9656b2ca145ef22bc260058c1cbb5e193562
Terence Faid JABO <faidterence@outlook.com>
2024-04-06 14:22:15 +0200

    feat(database):postgress connection configured

---
+++ src/main/java/com/faidterence/farmManagementSystem/FarmManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.faidterence.farmManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/faidterence/farmManagementSystem/schema/Property.java
@@ -0,0 +1,5 @@
+package com.faidterence.farmManagementSystem.schema;
+
+public class Property {
+
+}
