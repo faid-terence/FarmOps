1acc807e256614e9d66db48360cde860b2bdf67c
Terence Faid JABO <faidterence@outlook.com>
2024-04-06 22:40:14 +0200

    feat(controllers): create PropertyController

---
+++ src/main/java/com/faidterence/farmManagementSystem/controllers/PropertyController.java
@@ -0,0 +1,42 @@
+package com.faidterence.farmManagementSystem.controllers;
+
+
+import com.faidterence.farmManagementSystem.dto.CreatePropertyDTO;
+import com.faidterence.farmManagementSystem.schema.Property;
+import com.faidterence.farmManagementSystem.services.PropertyService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/properties")
+public class PropertyController {
+
+    private PropertyService propertyService;
+
+    @PostMapping
+    public ResponseEntity<CreatePropertyDTO> addProperty(@RequestBody CreatePropertyDTO createPropertyDTO) {
+        Property savedProperty = propertyService.createProperty(createPropertyDTO);
+
+        CreatePropertyDTO responseDto = new CreatePropertyDTO();
+        responseDto.setName(savedProperty.getName());
+        responseDto.setLocation(savedProperty.getLocation());
+        responseDto.setDescription(savedProperty.getDescription());
+        responseDto.setCategory(savedProperty.getCategory());
+        responseDto.setStatus(savedProperty.getStatus());
+        responseDto.setPrice(savedProperty.getPrice());
+        responseDto.setQuantity(savedProperty.getQuantity());
+        responseDto.setOwner(savedProperty.getOwner());
+        responseDto.setContact(savedProperty.getContact());
+        responseDto.setDate(savedProperty.getDate());
+
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+}
